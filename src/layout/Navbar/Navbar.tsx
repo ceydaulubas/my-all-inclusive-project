e4024f27098ede464fed89a3a28905986d8d0ef4
ceydaulubas <ceyda.ulubas@gmail.com>
2024-03-28 18:53:24 +0300

    created-navbar-style

--- src/layout/Navbar/Navbar.tsx
+++ src/layout/Navbar/Navbar.tsx
@@ -1,22 +1,65 @@
-import React from "react";
-import { useState } from "react";
-import "./Navbar.scss";
+// Navbar.tsx
 
-function Navbar() {
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
+import {
+  LeftOutlined,
+  RightOutlined,
+  ShoppingCartOutlined,
+  ScheduleOutlined,
+  PlusOutlined,
+  CalendarOutlined,
+  ExperimentOutlined,
+  MedicineBoxOutlined,
+  CarOutlined,
+  HomeOutlined
+} from "@ant-design/icons";
+
+interface NavItem {
+  title: string;
+  icon: JSX.Element;
+  path: string;
+}
+
+const Navbar: React.FC = () => {
+  const iconSize = '22px';
+  const [isNavbarPopupOpen, setIsNavbarPopupOpen] = useState<boolean>(false);
+
+  const toggleNavbar = () => {
+    setIsNavbarPopupOpen(!isNavbarPopupOpen);
+  };
+
+  const getIconWithSize = (icon: JSX.Element) => {
+    return React.cloneElement(icon, { style: { fontSize: iconSize, color: '#555' } });
+  };
+
+  const navItems: NavItem[] = [
+    { title: "Overview", icon: getIconWithSize(<HomeOutlined />), path: "/overview" },
+    { title: "Shopping List", icon: getIconWithSize(<ShoppingCartOutlined />), path: "/shopping-list" },
+    { title: "Tasks", icon: getIconWithSize(<ScheduleOutlined />), path: "/tasks" },
+    { title: "Recipes", icon: getIconWithSize(<PlusOutlined />), path: "/recipes" },
+    { title: "Meal Plan", icon: getIconWithSize(<ExperimentOutlined />), path: "/meal-plan" },
+    { title: "Calendar", icon: getIconWithSize(<CalendarOutlined />), path: "/calendar" },
+    { title: "Pill List", icon: getIconWithSize(<MedicineBoxOutlined />), path: "/pill-list" },
+    { title: "Travel Plan", icon: getIconWithSize(<CarOutlined />), path: "/travel-plan" }
+  ];
+
   return (
-    <div className="navbar-container">
-      <div>
-        <p>Overview</p>
-        <p>Shopping List</p>
-        <p>Tasks</p>
-        <p>Recipes</p>
-        <p>Meal Plan</p>
-        <p>Calender</p>
-        <p>Pill List</p>
-        <p>Travel Plan</p>
+    <div className={`navbar-container ${isNavbarPopupOpen ? 'open' : 'closed'}`}>
+      <div className="nav-items">
+        {navItems.map((item, index) => (
+          <Link key={index} to={item.path} className="nav-item">
+            {item.icon}
+            {isNavbarPopupOpen && <span>{item.title}</span>}
+          </Link>
+        ))}
+      </div>
+      <div className="toggle-icon" onClick={toggleNavbar}>
+        {isNavbarPopupOpen ? <LeftOutlined /> : <RightOutlined />}
       </div>
     </div>
   );
-}
+};
 
 export default Navbar;
