d1fd90af217c10fd7f7aa4f2cf94dc7a8d38adf0
ceydaulubas <ceyda.ulubas@gmail.com>
2024-04-15 23:56:47 +0300

    collect-all-weather-api-in-one-file

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,5 +1,4 @@
-import { fetch5DaysWeatherApi } from "./fetch5DaysWeatherApi ";
-import { fetchCurrentWeatherApi } from "./fetchCurrentWeatherApi";
+import { fetchCurrentWeather, fetch5DaysWeather} from "./fetchWeatherApi";
 import { fetchRandomQuoteApi } from "./fetchRandomQuoteApi";
 
-export { fetch5DaysWeatherApi, fetchCurrentWeatherApi, fetchRandomQuoteApi };
+export { fetchCurrentWeather, fetch5DaysWeather, fetchRandomQuoteApi };
