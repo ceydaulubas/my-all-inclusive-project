a7b341233b43953707de1b7914a9d5863d8ddb39
ceydaulubas <ceyda.ulubas@gmail.com>
2024-04-17 23:58:42 +0300

    delete-weather-redux

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { navbarPopupSlice } from "./navbarPopup/navbarPopupSlice";
 import { quoteSlice } from "./quoteSlice";
-import weatherSlice  from "./weatherSlice";
 
 export const store = configureStore({
     reducer: {
         navbarPopup: navbarPopupSlice.reducer,
         quote: quoteSlice.reducer,
-        weather : weatherSlice,
     },
     });
 
