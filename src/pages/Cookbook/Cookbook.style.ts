8553d9df8c54a3c986c04f71b7ad1430394e946a
ceydaulubas <ceyda.ulubas@gmail.com>
2024-10-13 23:36:49 +0300

    add-cookbar-page-card-part

--- src/pages/Cookbook/Cookbook.style.ts
+++ src/pages/Cookbook/Cookbook.style.ts
@@ -1,10 +1,18 @@
 import styled from 'styled-components';
 
 export const CookBookContainer = styled.div`
-    height: 100%;
+    background-color: grey;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 20px;
+    background-color: #f8f3f3;
+    margin: 80px;
+`;
+
+export const CardImage = styled.img`
     width: 100%;
-
-    background-color: blue;
-
-    color: red;
+    height: 100%;
+    object-fit: cover;
+    display: block;
 `;
--- src/pages/Cookbook/Cookbook.tsx
+++ src/pages/Cookbook/Cookbook.tsx
@@ -1,13 +1,50 @@
-import { useState } from 'react';
-import { CookBookContainer } from './Cookbook.style';
+import { useEffect, useState } from 'react';
+import { CookBookContainer, CardImage } from './Cookbook.style';
 import { QuoteDisplay } from '../../components/index';
 
-interface CookbookProps {}
+// Import Api
+import { fetchRandomRecipesApi } from '../../api/index';
+import { Card } from 'antd';
+const { Meta } = Card;
 
-const Cookbook = (props: CookbookProps) => {
-    const [state, setstate] = useState('');
+interface CookbookProps {
+    id: number;
+    title: string;
+    image: string;
+}
 
-    return <CookBookContainer>Cookbook</CookBookContainer>;
+const Cookbook = () => {
+    const [recipes, setRecipes] = useState<CookbookProps[] | null>(null);
+    const [error, setError] = useState<string>('');
+    const [search, setSearch] = useState<string>('');
+    const [searchResults, setSearchResults] = useState<CookbookProps[] | null>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await fetchRandomRecipesApi();
+                setRecipes(data.recipes);
+            } catch (error) {
+                setError('Error fetching random recipes data');
+            }
+        };
+        fetchData();
+    }, []);
+
+    console.log('recipes', recipes);
+
+    return (
+        <CookBookContainer>
+            {/* <h1>Cookbook</h1> */}
+            {recipes &&
+                recipes.map((recipe: CookbookProps) => (
+                    <Card style={{ width: 300 }} key={recipe.id}>
+                        <CardImage src={recipe.image} alt={recipe.title} />
+                        <Meta title="Card title" description={recipe.title} />
+                    </Card>
+                ))}
+        </CookBookContainer>
+    );
 };
 
 export default Cookbook;
