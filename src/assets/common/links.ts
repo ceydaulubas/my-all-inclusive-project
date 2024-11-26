0985f453b7808093290da2551d5b4ddba22980a7
ceydaulubas <ceyda.ulubas@gmail.com>
2024-11-26 23:55:56 +0300

    add_weather_condition_images_links

---
+++ src/assets/common/links.ts
@@ -0,0 +1,26 @@
+export const weatherConditionsImage = {
+    clearMorning:
+        'https://res.cloudinary.com/dxqyvjf5r/image/upload/v1732564436/weather_condition_background_images/alex-machado-80sv993lUKI-unsplash_nprd93.jpg',
+    clearNight:
+        'https://media.istockphoto.com/id/178411641/photo/tragic-night-sky-with-a-full-moon.jpg?s=612x612&w=0&k=20&c=tT1-CJ82eB_gRiV06jw8zTAUJ5yNh-euVWKmmVQbCRw=',
+    scatteredCloudsMorning:
+        'https://res.cloudinary.com/dxqyvjf5r/image/upload/v1732653082/weather_condition_background_images/4_cjs90j.jpg',
+    scatteredCloudsNight:
+        'https://res.cloudinary.com/dxqyvjf5r/image/upload/v1732653082/weather_condition_background_images/5_ko3ugx.jpg',
+    rainMorning:
+        'https://res.cloudinary.com/dxqyvjf5r/image/upload/v1732653082/weather_condition_background_images/6_fshp46.jpg',
+    rainNight:
+        'https://res.cloudinary.com/dxqyvjf5r/image/upload/v1732653082/weather_condition_background_images/7_pqyier.webp',
+    thunderstormMorning:
+        'https://res.cloudinary.com/dxqyvjf5r/image/upload/v1732653083/weather_condition_background_images/8_rcsiwd.jpg',
+    thunderstormNight:
+        'https://res.cloudinary.com/dxqyvjf5r/image/upload/v1732653083/weather_condition_background_images/9_cft2f4.webp',
+    snowMorning:
+        'https://res.cloudinary.com/dxqyvjf5r/image/upload/v1732653083/weather_condition_background_images/10_t4lvxg.jpg',
+    snowNight:
+        'https://res.cloudinary.com/dxqyvjf5r/image/upload/v1732653083/weather_condition_background_images/11_dablo4.jpg',
+    mistMorning:
+        'https://res.cloudinary.com/dxqyvjf5r/image/upload/v1732653083/weather_condition_background_images/12_fgbnhp.jpg',
+    mistNight:
+        'https://res.cloudinary.com/dxqyvjf5r/image/upload/v1732653083/weather_condition_background_images/13_alpgdd.jpg',
+};
